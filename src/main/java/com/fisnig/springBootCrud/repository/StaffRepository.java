5acf77335e836660da1a2c0c1c1d251d5d6519eb
david <egbunumalikdavid@gmail.com>
2025-05-28 07:03:51 +0200

    Create StaffRepository.java

---
+++ src/main/java/com/fisnig/springBootCrud/repository/StaffRepository.java
@@ -0,0 +1,12 @@
+package com.fisnig.springBootCrud.repository;
+
+import com.fisnig.springBootCrud.model.Staff;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StaffRepository extends CrudRepository<Staff, Long> {
+
+	
+	
+
+	
+}
