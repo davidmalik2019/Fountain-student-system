42fc4a68e44e347d0ba4c0ad34bbdb6a1d75a627
david <egbunumalikdavid@gmail.com>
2025-05-28 07:03:44 +0200

    Create CourseRepository.java

---
+++ src/main/java/com/fisnig/springBootCrud/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.fisnig.springBootCrud.repository;
+
+import com.fisnig.springBootCrud.model.Course;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+	
+	
+
+	
+}
